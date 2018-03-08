c6c16f2511bfba37a1070925b8bd136e78bfe6af
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-03-08 14:48:48 +0100

    create colored rectangle

--- zul/src/rectangle/Frame.java
+++ zul/src/rectangle/Frame.java
@@ -1,41 +1,56 @@
-/*package rectangle;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics2D;
+package rectangle;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
 
-import javax.swing.JFrame;
+class Frame extends JFrame{
 
-
-public class Frame extends JFrame{
-
+    /**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
 	public static final Color colors[] = {new Color(50, 226, 241),
 	        new Color(244, 222, 14), new Color(140, 18, 251), new Color(255, 0, 128)};
+	public static int xval = 0;
+	public static int yval = 0;
+	public static int strokeval = 20;
+	public static int xsizeFrame = 450;
+	public static int ysizeFrame = 450;
+	public static int xMidFrame = xsizeFrame/2;
+	public static int yMidFrame = ysizeFrame/2;
+	
+	public Frame(){
+        JPanel panel=new JPanel();
+        getContentPane().add(panel);
+        setSize(xsizeFrame,ysizeFrame);
 
-	public Frame()
-	{
-		setTitle("ColorSwitch");
-		setSize(600,600);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
-	}
-	
-	public void paint(Graphics2D g)
-	{
-		g.setStroke(new BasicStroke(20));
-		g.setColor(colors[0]);
-		g.drawLine(100, 100, 100, 150);
-		
-		
-	}
-	
-	public static void main(String[] args)
-	{
-		Frame f = new Frame();
-		
-		
-	}
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(strokeval));
+        
+        g2.setColor(colors[0]);
+        Line2D lin2 = new Line2D.Float(xMidFrame-50-strokeval,yMidFrame-50,xMidFrame-50-strokeval,yMidFrame+50);
+        g2.draw(lin2);
+        
+        g2.setColor(colors[1]);
+        Line2D lin1 = new Line2D.Float(xMidFrame-50,yMidFrame-50-strokeval,xMidFrame+50,yMidFrame-50-strokeval);
+        g2.draw(lin1);
+        
+        g2.setColor(colors[2]);
+        Line2D lin3 = new Line2D.Float(xMidFrame+50+strokeval,yMidFrame-50,xMidFrame+50+strokeval,yMidFrame+50);
+        g2.draw(lin3);
+        
+        g2.setColor(colors[3]);
+        Line2D lin4 = new Line2D.Float(xMidFrame-50,yMidFrame+50+strokeval,xMidFrame+50,yMidFrame+50+strokeval);
+        g2.draw(lin4);
+       
+    }
+
+    public static void main(String []args){
+        Frame s=new Frame();
+        s.setVisible(true);
+    }
 }
-*/
